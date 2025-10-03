c01cf98cebda0da17c06659020d7a3e4f7dd0d0b
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-10-03 10:05:57 -0300

    variables

---
+++ js/aula5/app.js
@@ -0,0 +1,20 @@
+/* 3 Formas
+
+- var
+- let
+- const
+
+*/
+
+var profissao = 'webdesigner';
+console.log(profissao);
+
+let endereco = 'Santa Catarina'
+console.log(endereco)
+
+const nome = 'Abraham'
+console.log(nome)
+
+//var -> variavel global / reconhecido por todo o codigo
+
+//let -> variavel local / reconhecido apenas dentro do bloco de chaves em que ele está inserido
