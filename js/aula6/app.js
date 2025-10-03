44ddeaef49f8158ac1e642d30616668947af2c30
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-10-03 10:31:40 -0300

    dataTypes

---
+++ js/aula6/app.js
@@ -0,0 +1,24 @@
+/* 3 Formas
+
+- var
+- let
+- const
+
+*/
+
+let proffisao = "do job"; // string -> Texto envolvido por aspas simples, duplas ou crase
+let age = 26; // numero inteiro -> int
+let altura = 1.76 // float -> ponto flutuante, mas também é considerado um type of number
+
+
+//arrays ou vetores
+let name = ['marcos', 'henrique']
+let ages = [22, 23];
+
+// objetos -> representados por {chave:valor}
+
+let aluno = {
+    nome: 'bruno',
+    age: 20,
+    year: 3
+}
