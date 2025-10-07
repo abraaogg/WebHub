5babf00fbd26ea06f97a4d8bc0712da4436fd7ca
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-10-07 17:29:51 -0300

    array

---
+++ js/aula14-arrays/app.js
@@ -0,0 +1,32 @@
+// Arrays -> vetores, matrizes
+
+// Estrutura de dados para armazenar um conjunto de elementos -> strings, numeors, booleans
+
+let frutas = ["Banana", "Apple", "Watermelon"]; // array tamanho 3
+console.log(frutas);
+
+//index [0], [1], [2]
+
+let num = new Array(2, 8, 7);
+console.log(num);
+
+//Tamanho of an array -> length
+console.log(num.length)
+
+
+let ultimoElementoArray = num.length - 1
+console.log(ultimoElementoArray) // Mostrar valor do index
+
+console.log(num [ultimoElementoArray]) // Mostrar quem é o ultimo valor do array
+
+
+
+
+
+let arrayVazio = [];
+
+arrayVazio[0] = "Beauty"
+
+console.log(arrayVazio)
+
+
