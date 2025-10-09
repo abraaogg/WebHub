80fbd75e74288b1c24f504ed1e388943c63bfc55
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-10-09 15:47:20 -0300

    Update app.js

--- js/aula21/app.js
+++ js/aula21/app.js
@@ -2,12 +2,22 @@
 
 // cenário: Filtrar pessoas que são maiores de idade
 
-const age = [23, 12, 8, 35]
+const age = [23, 12, 8, 35];
 
 // const pessoasAdulta = agePessoas.filter()
 
-function numeroMaior18(numero){
-    return numero >= 18; // true ou false
+function numeroMaior18(numero) {
+  return numero >= 18; // true ou false
 }
 
-console.log(numeroMaior18(19))
+console.log(numeroMaior18(19));
+
+let names = ["math", "gab", "ana", "jess"];
+
+let letra = names.filter(comecaComM);
+
+function comecaComM(name) {
+  return name.startsWith("m");
+}
+
+console.log(letra);
