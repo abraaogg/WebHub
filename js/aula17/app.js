ce5f7f46162922b294c0afd5d208c0e699206f45
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-10-09 11:26:02 -0300

    splice

---
+++ js/aula17/app.js
@@ -0,0 +1,12 @@
+
+let nomes = ["gustavo", "marcos", "matheus"];
+
+//remover o ultimo elemento do array
+nomes.pop();
+console.log(nomes);
+
+//remover o primeiro elemento do array
+nomes.shift();
+console.log(nomes);
+
+delete nomes[2] // remove o valor do indice
---
+++ js/aula18/app.js
@@ -0,0 +1,14 @@
+//Slice -> fatiar um array e gerar um novo array, e não modifica o array original
+
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// slice(ondeVamosComecaraFatiar, ateOnde)
+
+let numsFatiados = nums.slice(3, 6); // do indice 3 ate o 5
+
+console.log(nums);
+console.log(numsFatiados);
+
+let names = ["gustavo", "marcos", "rafael", "henrique", "juliano"];
+let namesFatiados = names.slice(1, 3)
+console.log(namesFatiados)
