e1618203ef8911051eb61ff9410e107fba48757f
abraaogg <153078194+abraaogg@users.noreply.github.com>
2025-10-14 09:45:20 -0300

    template-strings

---
+++ js/aula23/app.js
@@ -0,0 +1,17 @@
+// Template Strings
+
+// Concatenção -> unir duas ou mais strings
+
+let msg1 = "olá";
+
+let user = "Gabe";
+
+let age = 19;
+
+let fullmsg = msg1 + " " + user;
+
+console.log(fullmsg);
+
+// Template Strings -> maneira mais moderna de concatenar strings
+
+console.log(`${msg1} ${user} você tem ${age} anos`);
