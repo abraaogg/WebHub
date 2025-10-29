3257c21c89639c55de0734b82c5b43350d65a937
abraaogg <abraaogoncalves1022@gmail.com>
2025-10-29 14:56:09 -0300

    react

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,18 +1,31 @@
 import "./App.css";
 import Contador from "./components/Contador";
-import deku from "./assets/deku.jpg"
+import deku from "./assets/deku.jpg";
 
 function App() {
-  const frutas = ["Strawberry", "Banana", "Apple"]
+  //  const frutas = ["Strawberry", "Banana", "Apple"]
+  const tarefas = [
+    {id: 1,titulo: "Estudar React", feita: true },
+    {id: 2, titulo: "Estudar Js", feita: false },
+    {id: 3, titulo: "Estudar College", feita: true }
+  ];
   return (
- /*    <div>
+    <div>
+      <h1>Lista de Tarefas</h1>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li className={iftarefa.feita ? "feita" : ""} key={tarefa.id}>{tarefa.titulo}</li>
+        ))}
+      </ul>
+    </div>
+    /*    <div>
       <img src={deku} alt="Midoriya" />
 
       <Contador /> 
     </div> */
-    <ul>
+    /*     <ul>
       {frutas.map(frutas => <li>{frutas}</li>)}
-    </ul>
+    </ul> */
   );
 }
 
