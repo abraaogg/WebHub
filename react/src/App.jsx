26b1dd414b298ef235f6c99a29efac7738e21c96
abraaogg <abraaogoncalves1022@gmail.com>
2025-10-29 10:58:20 -0300

    image-react

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Contador from "./components/Contador";
+import deku from "./assets/deku.jpg"
 
 function App() {
   return (
     <div>
+      <img src={deku} alt="Midoriya" />
       <Contador />
     </div>
   );
