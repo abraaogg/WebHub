196dce962dae771de4f2052817d6cecebfb90fca
abraaogg <abraaogoncalves1022@gmail.com>
2025-12-17 10:19:33 -0300

    aula12

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,18 +3,24 @@
 import "./App.css";
 import Counter from "./components/Counter";
 import Destructuring from "./components/Destructuring";
+import Form from "./components/form";
 import Lista from "./components/Lista";
 import ProfileCard from "./components/ProfileCard";
 import Titulo1 from "./components/titulo1";
 
+// state Lift -> Elevar o estado
+
 function App() {
   return (
     <>
-     {/* {/*  {/* <ProfileCard />
-      <Titulo1 /> {" "}
+      {/*
+      <ProfileCard />
+      <Titulo1 />
       <Lista />
-      <Counter /> */}
-      <Destructuring name="Abraham" age={22}/>
+      <Counter /> 
+      <Destructuring name="Abraham" age={22} />
+      */}
+      <Form />
     </>
   );
 }
---
+++ react/src/components/Form.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+// onChange, same as onClick
+
+export const Form = () => {
+  const [name, setName] = useState("Nome");
+  function changeForm(e) {
+    setName(e.target.value);
+  }
+
+  function sendForm() {
+    alert(`Informações enviadas: ${name}`);
+  }
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <form onSubmit={sendForm}>
+        <label htmlFor="name">Name </label>
+        <input onChange={changeForm} type="text" id="name" name="name" />
+
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
