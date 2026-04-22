b71361879289fcb53a942b2ae37edc1b6a9a528e
abraaogg <abraaogoncalves1022@gmail.com>
2026-04-22 16:19:33 -0300

    GSAP

---
+++ gsap/script.js
@@ -0,0 +1,16 @@
+/* gsap.to(".quadrado", {
+  rotation: 100,
+  x: 100,
+  duration: 1,
+  backgroundColor: "rgb(235, 63, 63)",
+});
+
+//animate ".quadrado" from an opacity of 0 to an opacity of 0.5
+gsap.fromTo(".bquadradox", { opacity: 0 }, { opacity: 0.5, duration: 1 }); */
+
+gsap.fromTo(".circle", {
+    x: -300,
+}, {
+    x: 100,
+    duration: 3
+})
