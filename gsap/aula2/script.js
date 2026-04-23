03661506620984aca9a8d636c5581772bc3f68b3
abraaogg <abraaogoncalves1022@gmail.com>
2026-04-23 15:18:34 -0300

    gsap

---
+++ gsap/aula2/script.js
@@ -0,0 +1,42 @@
+//Timeline
+
+/* gsap.to(".quadrado",
+    {
+        x: 100,
+        y: -200,
+        backgroundColor: "green",
+        duration: 2,
+        ease: "elastic.out(1,0.3)" 
+    }
+)
+gsap.to(".quadrado",
+    {
+        x: -50,
+        y: 100,
+        backgroundColor: "crimson",
+        duration: 2,
+        ease: "elastic.out(1,0.3)" ,
+        delay: 2
+    }
+)
+ */
+const tl = gsap.timeline();
+
+tl.to(".quadrado", {
+  x: 100,
+  y: -200,
+  backgroundColor: "green",
+  duration: 2,
+  ease: "bounce.out(1,0.3)",
+  stagger: 0.5,
+}).to(".quadrado", {
+  x: -50,
+  y: 100,
+  backgroundColor: "crimson",
+  duration: 2,
+  ease: "sine.out(1,0.3)",
+  stagger: 0.5,
+});
+
+const botaoPlay = document.querySelector(".play");
+const botaoPause = document.querySelector(".pause");
