b23b54224ca422a890e21bc6b0945f9600186e9f
abraaogg <abraaogoncalves1022@gmail.com>
2026-05-08 10:30:55 -0300

    gsap

--- gsap/aula4/script.js
+++ gsap/aula4/script.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".quadrado", {
+/* gsap.from(".quadrado", {
   opacity: 0,
   y: 40,
   filter: "blur(20px)",
@@ -10,5 +10,38 @@ gsap.from(".quadrado", {
     end: "100% bottom",
     markers: true,
     scrub: 1
+  },
+}); */
+
+const triggerSettings = {
+  trigger: ".container",
+  start: "top center",
+  end: "bottom center",
+  markers: true,
+};
+
+
+gsap.to(".quadrado1", {
+  rotation: 360,
+  scale: 0.8,
+  scrollTrigger: {
+    scrub: 2,
+  },
+});
+
+gsap.to(".quadrado2", {
+  borderRadius: "50%",
+  rotation: 360,
+  scale: 1.2,
+  margin: "40px",
+  scrollTrigger: {
+    scrub: 4,
+  },
+});
+
+gsap.to(".quadrado3, .quadrado4", {
+  rotation: -360,
+  scrollTrigger: {
+    scrub: 2,
   },
 });
