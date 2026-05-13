877352272aeb67767e32d004f07a8037f36674e1
abraaogg <abraaogoncalves1022@gmail.com>
2026-05-13 09:45:09 -0300

    gsap

--- gsap/aula6 exercício/script.js
+++ gsap/aula6 exercício/script.js
@@ -1,12 +1,26 @@
-gsap.registerPlugin(scrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
+
+let valorMaskSize = "4000vw";
 
 gsap.to(".mask", {
-  maskSize: "2000vw",
+  maskSize: valorMaskSize,
+  maskPosition: "52% 40%", // horizontal, vertical position
   scrollTrigger: {
     trigger: ".container",
     pin: true,
     start: "top top",
     end: "bottom 20%",
-    scrub: 1
+    scrub: 1,
+  },
+});
+
+gsap.from(".content", {
+  opacity: 0,
+  y: 100,
+  scrollTrigger: {
+    trigger: ".content",
+    start: "top top",
+    end: "bottom 20%",
+    scrub: 1,
   },
 });
